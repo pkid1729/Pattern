b5d750797ad3a87a704b9bc281a8923d32203a21
pkid1729 <30475130+pkid1729@users.noreply.github.com>
2018-09-08 01:44:24 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(i);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+            // Console.WriteLine("Hello World!");
+            for (int k = 9; k>=1; --k)
+            {
+                for (int l = 1; l <= k; ++l)
+                {
+                    Console.Write(k);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
